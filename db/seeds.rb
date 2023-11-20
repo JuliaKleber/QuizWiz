e68252440ce4f7e1fb30c734bc4f9fbdfd3e38a3
Julia Kleber <julia.kleber@gmx.net>
2023-11-20 16:16:30 +0100

    added users and questions to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+User.destroy_all
+Quiz.destroy_all
+Question.destroy_all
+QuizQuestion.destroy_all
+
+
+
+
+
+
+# 5.times do
+#   Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address,
+#                     phone_number: Faker::PhoneNumber.phone_number,
+#                     category: %w[chinese italian japanese french belgian].sample)
+# end
+
+User.create(username: 'Aisiri')
+User.create(username: 'Bilal')
+User.create(username: 'Elena')
+User.create(username: 'Julia')
+
+puts 'Creating users...'
+
+Question.create(content: 'What is the capital of France?', choice_one: 'Paris', choice_two: 'London')
+Question.create(content: 'Which planet is known as the "Red Planet"?', choice_one: 'Mars', choice_two: 'Venus')
+Question.create(content: 'What is the largest mammal in the world?', choice_one: 'Elephant', choice_two: 'Blue Whale')
+Question.create(content: 'In which year did Christopher Columbus reach the Americas?', choice_one: '1492', choice_two: '1776')
+Question.create(content: 'What is the main ingredient in guacamole?', choice_one: 'Avocado', choice_two: 'Tomato')
+Question.create(content: 'Who wrote the play "Romeo and Juliet"?', choice_one: 'William Shakespeare', choice_two: 'Jane Austin')
+Question.create(content: 'Which of the following is a primary color?', choice_one: 'Green', choice_two: 'Blue')
+Question.create(content: 'What is the capital of Japan?', choice_one: 'Beijing', choice_two: 'Tokyo')
+Question.create(content: 'What is the currency of the United Kingdom?', choice_one: 'Euro', choice_two: 'Pound Sterling')
+Question.create(content: 'What is the largest ocean on Earth?', choice_one: 'Atlantic Ocean', choice_two: 'Pacific Ocean')
+
+puts 'Creating questions...'
+
+puts 'Creating quizzes...'
+
+puts 'Creating connections between questions and quizzes...'
