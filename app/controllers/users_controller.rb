a18525758fb27169fef40781040dfd6484725c14
ehiaghe aisiri <ehiaghe.aisiri@gmail.com>
2023-11-22 12:05:43 +0100

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def login
   end
+
+  # def create
+  #   @user = user.new(user_params)
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+  # def user_params
+  #   params.require(:user).permit(:email, :encrypted_password, :user_name, :photo)
+  # end
 end
